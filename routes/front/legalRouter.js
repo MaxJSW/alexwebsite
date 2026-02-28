bc6137134095aeebcc9c04bd857f3bc13c34662a
Max Mouza <maxencemouza@protonmail.com>
2026-02-28 13:20:16 +0100

    mise en place de la page conditions generales et mentions legales

--- routes/front/legalRouter.js
+++ routes/front/legalRouter.js
@@ -6,10 +6,9 @@ const fs = require('fs');
 const path = require('path');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
-
+const db = require('../db');
 
 dotenv.config();
-
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -26,13 +25,30 @@ const storage = multer.diskStorage({
 
 router.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-
 router.get('/', (req, res) => {
-    res.render('conditions-generales', {
-  });
+    const userId = 10;
+
+    const querySocialLinks = `
+        SELECT 
+            usl.social_media_link,
+            sn.name AS network_name
+        FROM user_social_links usl
+        LEFT JOIN social_network sn ON usl.social_network_id = sn.id
+        WHERE usl.user_id = ?
+        AND usl.social_media_link IS NOT NULL
+        AND usl.social_media_link != ''
+    `;
+
+    db.query(querySocialLinks, [userId], (err, socialLinks) => {
+        if (err) {
+            console.error('Erreur récupération social links:', err);
+            return res.redirect('/erreur');
+        }
+
+        res.render('conditions-generales', {
+            socialLinks
+        });
+    });
 });
-
-
 
 module.exports = router;
